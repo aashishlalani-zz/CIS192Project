696383ca373819c472b9ca729ac41b366999cb40
Anvita Achar <anvita.achar@gmail.com>
2015-12-09 00:19:22 -0500

    gui and email update

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
-
-from Tkinter import Tk, BOTH
-from ttk import Frame, Button, Style
+from Tkinter import Tk, BOTH, StringVar
+from ttk import Frame, Button, Style, Label, Entry
+from send_emails import send_email
 
 class Example(Frame):
   
@@ -14,21 +14,29 @@ class Example(Frame):
         
     def initUI(self):
       
-        self.parent.title("Quit button")
+        self.parent.title("One Time Pad Generator")
         self.style = Style()
         self.style.theme_use("default")
-
         self.pack(fill=BOTH, expand=1)
+        keyButton = Button(self, text="Generate Key",
+            command=self.generateKey)
+        keyButton.place(x=240, y=240)
+      
+        
+    def generateKey(self):
+        l1 = Label(self, text = "1249098")
+        l1.place(x=10, y=10)
+        entryfield = Entry(self, width=7, textvariable=StringVar())
+        entryfield.place(x=100, y=300)
 
-        quitButton = Button(self, text="Quit",
-            command=self.quit)
+        quitButton = Button(self, text="Email",
+        command=send_email)
         quitButton.place(x=50, y=50)
-
 
 def main():
   
     root = Tk()
-    root.geometry("250x150+300+300")
+    root.geometry("500x500+300+300")
     app = Example(root)
     root.mainloop()  
 
--- send_emails.py
+++ send_emails.py
@@ -1,26 +1,20 @@
 import smtplib
 
-fromaddr = 'lol@gmail.com'
-toaddrs  = 'lol@gmail.com'
-fromline = 'From: '+ fromaddr
-toline = 'To: '+ toaddrs 
-msg = "\r\n".join([
-  "From: user_me@gmail.com",
-  "To: user_you@gmail.com",
-  "Subject: Just a message",
-  "",
-  "Why, oh why"
-  ])
+
+username = "onetimepadcis192@gmail.com"
+toaddrs  = "anvita.achar@gmail.com"
 
 
-# Credentials (if needed)
-username = 'anvita.achar'
-password = 'ADD YOUR PASSWORD'
-
-# The actual mail send
-server = smtplib.SMTP('smtp.gmail.com:587')
-server.ehlo()
-server.starttls()
-server.login(username,password)
-server.sendmail(fromaddr, toaddrs, msg)
-server.quit()
+def send_email():
+    msg = "dagkadjhkljh"
+    # Credentials (if needed)
+    password = "0n3t1m3p4d"
+    
+    # The actual mail send
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+    server.login(username, password)
+    server.sendmail(username, toaddrs, msg)
+    server.quit()
