31300c8ab9868b8aad225d1f12914a369e562790
aashishlalani <aashishlalani@hntvlan569.1913.wlan.wireless-resnet.upenn.edu>
2015-12-17 15:06:14 -0500

    implemented input and output of one-time-pads. Encrypt/Decrypt now returns a tuple (as a list of length 2) that contains the result and also the point at which the the time you encrypt you should start encrypting/decrypting (An example is given in main). Therefore the encrypt/decrypt function also takes in the start point for the pad. If this is left blank, it will default to starting at 0. The key generator now does not need a msg anymore, instead you can specify a length in the argument or if you leave it blank it will create a key of length 1000 (if it runs out of keys during encryption/decryption it will automatically wrap around).
    Examples using all functions are given in main (uses the padOutput file
    for inputing a pad)

--- Cryptography.py
+++ Cryptography.py
@@ -9,117 +9,75 @@ Created on Dec 8, 2015
 
 @author: aashishlalani
 '''
-
-def Encrypt(string, pad):
+def Encrypt(string, pad, start = 0):
     
     stringInBin2 = ''
     for x in string:
-        #print x
         temp = ''.join(format(ord(x), 'b'))
-        
         while(len(temp) < 8):
             temp = "0" + temp
-        
         stringInBin2 = stringInBin2 + (temp);
         
-    #print "msgInBin: " + str(stringInBin2)
-    #print "lenof msgInBin: " + str(len(stringInBin2))
-    #print "pad: " + str(pad)
     res = []
+    nextStartPoint = 0;
     for i in range (0, len(stringInBin2)):
-        #print i
-        res.append(int(stringInBin2[i]) ^ pad[i])
-    
-    #print "res after xoring: " + str(res)
-    #print "len of res: " + str((len(res)))
+        res.append(int(stringInBin2[i]) ^ pad[(i + start) % len(pad)])
+        nextStartPoint = (i + start) % len(pad) + 1
     
     encrypted = ''
     count = 0;
     while (count < len(res)):
         temp = ''
         while(len(temp) < 8 and count < len(res)):
-            #print "count" + str(count)
             temp = temp + str(res[count])
             count += 1
-        #print temp
         encrypted = encrypted + chr(int(temp, 2))
         temp = ''
     
-    #print "encrypted msg: " + str(encrypted)
-    return encrypted
+    return [encrypted, nextStartPoint]
 
-
-#use encrypt for decrypt
-def Decrypt(string, pad):
-    '''
-    res = []
-    for i in range(0, len(string)):
-        char = string[i]
-        for j in range(0, 128):
-            if (pad[j] == char):
-                res.append(chr(j))
     
-    s = ''.join(res)
-    '''
-    print "starting decrypt!!!! __________________"
-    stringInBin2 = ''
-    for x in string:
-        print x
-        temp = ''.join(format(ord(x), 'b'))
-        
-        while(len(temp) < 8):
-            temp = "0" + temp
-        
-        stringInBin2 = stringInBin2 + (temp);
-        
-    print "msgInBin: " + str(stringInBin2)
-    print "lenof msgInBin: " + str(len(stringInBin2))
-    print "pad: " + str(pad)
-    return ''
-    pass
-    
-def GenerateKey(msg):
-    
-    
-    
+def GenerateKey(pad_len = 1000):
     key = []
-    msgInAscii = []
-    
-    
-    stringInBin = ''.join(format(ord(x), 'b') for x in msg)
-    
-    stringInBin2 = ''
-    for x in msg:
-        temp = ''.join(format(ord(x), 'b'))
-        while(len(temp) < 8):
-            temp = "0" + temp
-        
-        stringInBin2 = stringInBin2 + (temp);
-    
-    msg_len = len(stringInBin2)
-    #print "msg len " + str(msg_len)
-    '''
-    ascii = {i: chr(i) for i in range(0, 128)}
-    keys = list(ascii.keys())
-    random.shuffle(keys)
-    res = {keys[i]: ascii[i] for i in range(0, 128)}
-   
-    return res
-    '''
-    for i in range(0, msg_len):
+    for i in range(0, pad_len):
         key.append(random.SystemRandom().randint(0, 1))
 
     return key
     pass
 
+
+def outputPad(filename, pad):
+    with open(filename, 'w') as jsonfile:
+        json.dump(pad, jsonfile)
+    
+def inputPad(filename):
+    json_res = []
+    with open(filename) as jsonfile:
+        if (jsonfile is None):
+            raise Exception("No such file.")
+        try:
+            json_res = json.load(jsonfile)
+        except ValueError:
+            raise Exception('Invalid JSON.')
+    return json_res
+
 def main():
 
-    pad = GenerateKey("Hello hello hello")
+    pad = GenerateKey();
     
-    res = Encrypt("Hello hello hello", pad)
-    deRes = Encrypt(res, pad)
-    print res
-    print deRes
+    input = inputPad("padOutput");
+    
+    print input;
+    
+    resAndStartPoint = Encrypt("Hello hello hello", input, 2)
+    print resAndStartPoint[1]
+    resAndStartPoint2 = Encrypt("SUP SUP SUP", input, resAndStartPoint[1])
+    deRes = Encrypt(resAndStartPoint[0], input, 2)
+    deRes2 = Encrypt(resAndStartPoint2[0], input, deRes[1]);
+    print resAndStartPoint[0]
+    print resAndStartPoint2[0]
+    print deRes[0]
+    print deRes2[0]
 
 if __name__ == '__main__':
     main()  
